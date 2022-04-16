874df843ba52e098129c29f93f7a107cbde75763
Modar Nasser <modar1999@gmail.com>
2022-04-16 19:55:06 +0200

    examples: update events example

--- examples/02_events.cpp
+++ examples/02_events.cpp
@@ -12,28 +12,43 @@ int main() {
     while (window.isOpen()) {
         while (auto event = window.nextEvent()) {
             if (auto key_press = event->as<sogl::Event::KeyPress>()) {
-                std::cout << "KeyPress: " << key_press->key << " " << key_press->mods << std::endl;
+                std::cout << "KeyPress: " << std::hex
+                          << key_press->key << " "
+                          << key_press->mods << std::endl;
             }
             else if (auto key_release = event->as<sogl::Event::KeyPress>()) {
-                std::cout << "KeyRelease: " << key_release->key << " " << key_release->mods << std::endl;
+                std::cout << "KeyRelease: " << std::hex
+                          << key_release->key << " "
+                          << key_release->mods << std::endl;
             }
             else if (auto mouse_press = event->as<sogl::Event::MousePress>()) {
-                std::cout << "MousePress: " << mouse_press->button << " " << mouse_press->mods << std::endl;
+                std::cout << "MousePress: " << std::hex
+                          << mouse_press->button << " "
+                          << mouse_press->mods << std::endl;
             }
             else if (auto mouse_release = event->as<sogl::Event::MouseRelease>()) {
-                std::cout << "MouseRelease: " << mouse_release->button << " " << mouse_release->mods << std::endl;
+                std::cout << "MouseRelease: " << std::hex
+                          << mouse_release->button << " "
+                          << mouse_release->mods << std::endl;
             }
             else if (auto mouse_move = event->as<sogl::Event::MouseMove>()) {
-                std::cout << "MouseMove: " << mouse_move->x << " " << mouse_move->y << std::endl;
+                std::cout << "MouseMove: " << std::dec
+                          << mouse_move->x << " "
+                          << mouse_move->y << std::endl;
             }
             else if (auto scroll = event->as<sogl::Event::Scroll>()) {
-                std::cout << "Scroll: " << scroll->dx << " " << scroll->dy << std::endl;
+                std::cout << "Scroll: " << std::dec
+                          << scroll->dx << " "
+                          << scroll->dy << std::endl;
             }
             else if (auto resize = event->as<sogl::Event::Resize>()) {
-                std::cout << "Resize: " << resize->width << " " << resize->height << std::endl;
+                std::cout << "Resize: " << std::dec
+                          << resize->width << " "
+                          << resize->height << std::endl;
             }
             else if (auto drop = event->as<sogl::Event::Drop>()) {
-                std::cout << "Drop: " << drop->files.size() << " files" << std::endl;
+                std::cout << "Drop: "
+                          << drop->files.size() << " files" << std::endl;
             }
         }
         window.clear();
