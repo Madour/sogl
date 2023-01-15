5d7adcdd0a0d7d5b22c2e3bf286726a70987373b
Modar Nasser <modar1999@gmail.com>
2023-01-15 19:36:24 +0100

    cmake: Bundle glm inside the repo
    Fetching glm with cmake was downloading more than 50 MB.
    Bundling this depency should speed up the build without any drawbacks (glm is headers only)

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -1,7 +1,6 @@
 include(FetchContent)
 
-macro(setup_dependencies)
-    set(FETCHCONTENT_QUIET FALSE)
+macro(fetch_dependencies)
     if (NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Emscripten")
         FetchContent_Declare(
                 glew
@@ -11,11 +10,12 @@ macro(setup_dependencies)
                 GIT_PROGRESS TRUE
         )
         if(NOT glew_POPULATED)
-            message(STATUS "[sogl] Fetching GLEW 2.2.0")
+            message(STATUS "[sogl] Fetching GLEW 2.2.0...")
             FetchContent_Populate(glew)
             set(glew-cmake_BUILD_SHARED OFF CACHE INTERNAL "")
             set(glew-ONLY_LIBS ON CACHE INTERNAL "")
             add_subdirectory(${glew_SOURCE_DIR} ${glew_BINARY_DIR})
+            message(STATUS "[sogl] Configured GLEW 2.2.0")
         endif()
 
         FetchContent_Declare(
@@ -26,26 +26,14 @@ macro(setup_dependencies)
                 GIT_PROGRESS TRUE
         )
         if(NOT glfw_POPULATED)
-            message(STATUS "[sogl] Fetching GLFW 3.3.6")
+            message(STATUS "[sogl] Fetching GLFW 3.3.6...")
             FetchContent_Populate(glfw)
             set(GLFW_BUILD_EXAMPLES OFF CACHE INTERNAL "")
             set(GLFW_BUILD_TESTS OFF CACHE INTERNAL "")
             set(GLFW_BUILD_DOCS OFF CACHE INTERNAL "")
             set(GLFW_INSTALL OFF CACHE INTERNAL "")
             add_subdirectory(${glfw_SOURCE_DIR} ${glfw_BINARY_DIR})
+            message(STATUS "[sogl] Configured GLFW 3.3.6")
         endif()
-    endif()
-
-    FetchContent_Declare(
-            glm
-            GIT_REPOSITORY https://github.com/g-truc/glm
-            GIT_TAG 0.9.9.8
-            GIT_SHALLOW TRUE
-            GIT_PROGRESS TRUE
-    )
-    if(NOT glm_POPULATED)
-        message(STATUS "[sogl] Fetching glm 0.9.9.8")
-        FetchContent_Populate(glm)
-        add_subdirectory(${glm_SOURCE_DIR} ${glm_BINARY_DIR})
     endif()
 endmacro()
